99ddfe5835d3f71c1b04eb0dbc7ffda39dcc3f8c
MuzafferKadir <muzafferkadiryilmaz@gmail.com>
2021-01-14 23:34:27 +0300

    Layouts are created

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Navbar/>
+    <slot />
+  </div>
+</template>
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  name: 'App',
+  components: { Navbar }
+}
+</script>
